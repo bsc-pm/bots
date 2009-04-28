d14a793c0e24c17b7330c637beebe29d71352ed0
Alex Duran <alex.duran@bsc.es>
2009-04-28 15:41:49 +0000

    More headers changes
    git-svn-id: svn+ssh://mercurium.pc.ac.upc.edu/var/lib/svn/nbs@3385 39d7fdcd-100c-0410-a620-8454655e6f6e

--- omp-tasks/alignment/alignment.c
+++ omp-tasks/alignment/alignment.c
@@ -18,6 +18,8 @@
 /*  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA            */
 /**********************************************************************************************/
 
+/* Original code from the Application Kernel Matrix by Cray */
+
 #include <stdio.h>
 #include <stdlib.h> 
 #include <string.h>
--- omp-tasks/alignment/param.h
+++ omp-tasks/alignment/param.h
@@ -18,6 +18,8 @@
 /*  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA            */
 /**********************************************************************************************/
 
+/* Original code from the Application Kernel Matrix by Cray */
+
 char *amino_acid_order = "ABCDEFGHIKLMNPQRSTVWXYZ";
 char *amino_acid_codes = "ABCDEFGHIKLMNPQRSTUVWXYZ-";
 
