9097ab25af7d85e3a746c178c8d1239f0b28cfe5
Alex Duran <alex.duran@bsc.es>
2009-05-20 17:20:42 +0200

    Added serial directory to pack.sh; fixes #28

--- pack.sh
+++ pack.sh
@@ -2,7 +2,7 @@
 
 make clean
 mkdir bots
-cp -ar common/ config inputs/ omp-tasks/ run/ bots
+cp -ar common/ config inputs/ omp-tasks/ serial run/ bots
 cp -a configure Makefile bots
 rm -fr bots/omp-tasks/concom/ bots/omp-tasks/knapsack/ bots/inputs/knapsack/
 tar --exclude=*svn* -zcf bots-0.1.tar.gz bots
