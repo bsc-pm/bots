fd8651c1d7b76ea43af5294050ad9402760bf1ee
Alex Duran <alex.duran@bsc.es>
2009-05-27 16:31:47 +0200

    Serial FFT cleanup; closes #49

--- serial/fft/app-desc.h
+++ serial/fft/app-desc.h
@@ -19,6 +19,7 @@
 /**********************************************************************************************/
 
 #include "serial-app.h"
+#include "fft.h"
 
 #define BOTS_APP_NAME "FFT"
 #define BOTS_APP_PARAMETERS_DESC "Size=%d"
@@ -28,40 +29,7 @@
 #define BOTS_APP_DEF_ARG_SIZE 32*1024*1024
 #define BOTS_APP_DESC_ARG_SIZE "Matrix Size"
 
-/* our real numbers */
-typedef float REAL;
-
-/* Complex numbers and operations */
-typedef struct {
-     REAL re, im;
-} COMPLEX;
-
-#define c_re(c)  ((c).re)
-#define c_im(c)  ((c).im)
-
-void compute_w_coefficients(int n, int a, int b, COMPLEX * W);
-int factor(int n);
-void unshuffle(int a, int b, COMPLEX * in, COMPLEX * out, int r, int m);
-void fft_twiddle_gen1(COMPLEX * in, COMPLEX * out, COMPLEX * W, int r, int m, int nW, int nWdnti, int nWdntm);
-void fft_twiddle_gen(int i, int i1, COMPLEX * in, COMPLEX * out, COMPLEX * W, int nW, int nWdn, int r, int m);
-void fft_base_2(COMPLEX * in, COMPLEX * out);
-void fft_twiddle_2(int a, int b, COMPLEX * in, COMPLEX * out, COMPLEX * W, int nW, int nWdn, int m);
-void fft_unshuffle_2(int a, int b, COMPLEX * in, COMPLEX * out, int m);
-void fft_base_4(COMPLEX * in, COMPLEX * out);
-void fft_twiddle_4(int a, int b, COMPLEX * in, COMPLEX * out, COMPLEX * W, int nW, int nWdn, int m);
-void fft_unshuffle_4(int a, int b, COMPLEX * in, COMPLEX * out, int m);
-void fft_base_8(COMPLEX * in, COMPLEX * out);
-void fft_twiddle_8(int a, int b, COMPLEX * in, COMPLEX * out, COMPLEX * W, int nW, int nWdn, int m);
-void fft_unshuffle_8(int a, int b, COMPLEX * in, COMPLEX * out, int m);
-void fft_base_16(COMPLEX * in, COMPLEX * out);
-void fft_twiddle_16(int a, int b, COMPLEX * in, COMPLEX * out, COMPLEX * W, int nW, int nWdn, int m);
-void fft_unshuffle_16(int a, int b, COMPLEX * in, COMPLEX * out, int m);
-void fft_base_32(COMPLEX * in, COMPLEX * out);
-void fft_twiddle_32(int a, int b, COMPLEX * in, COMPLEX * out, COMPLEX * W, int nW, int nWdn, int m);
-void fft_unshuffle_32(int a, int b, COMPLEX * in, COMPLEX * out, int m);
-void fft_aux(int n, COMPLEX * in, COMPLEX * out, int *factors, COMPLEX * W, int nW);
 void fft(int n, COMPLEX * in, COMPLEX * out);
-int test_correctness(int n, COMPLEX *out1, COMPLEX *out2);
 
 #define BOTS_APP_INIT int i;\
      COMPLEX *in, *out1;\
@@ -75,11 +43,6 @@ int test_correctness(int n, COMPLEX *out1, COMPLEX *out2);
      }
 #define KERNEL_CALL fft(bots_arg_size, in, out1);
 #define KERNEL_FINI 
-
-#define KERNEL_SEQ_INIT
-#define KERNEL_SEQ_CALL
-#define KERNEL_SEQ_FINI
-
 
 #define KERNEL_CHECK BOTS_RESULT_NA;
 
