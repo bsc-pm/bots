5a57eac6b0735bd53ddc72906c1f97c36c28f8cb
Alex Duran <alex.duran@bsc.es>
2009-05-29 00:20:45 +0200

    Upgrade to version 1.0

--- pack.sh
+++ pack.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-VERSION=0.9
+VERSION=1.0
 
 make clean
 mkdir bots
 cp -ar common/ config inputs/ omp-tasks/ serial run/ bots
 cp -a configure Makefile bots
 rm -fr bots/omp-tasks/concom/ bots/omp-tasks/knapsack/ bots/inputs/knapsack/
-tar --exclude=*svn* -zcf bots-$VERSION.tar.gz bots
+tar --exclude=*svn* --exclude=*knapsack* -zcf bots-$VERSION.tar.gz bots
 rm -fr bots
