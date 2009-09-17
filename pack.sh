6cbfc895358498624554924923c96c9045f1de16
Alex Duran <alex.duran@bsc.es>
2009-09-17 16:10:49 +0200

    Upgraded version to 1.1b. Protect files while packing (closes #71)

--- pack.sh
+++ pack.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-VERSION=0.9
+VERSION=1.1b
 
 make clean
 mkdir bots
 cp -ar common/ config inputs/ omp-tasks/ serial run/ bots
 cp -a configure Makefile bots
-rm -fr bots/omp-tasks/concom/ bots/omp-tasks/knapsack/ bots/inputs/knapsack/
+rm -fr bots/omp-tasks/concom/ bots/omp-tasks/knapsack/ bots/inputs/knapsack/ bots/config/make.config
+find bots -type f -print0 | xargs -0 chmod a-w
 tar --exclude=*svn* -zcf bots-$VERSION.tar.gz bots
 rm -fr bots
